0fd4e9766e9e27d5bba86330af6655577683b55e
Tanjil Ahamed <tanjil113355@gmail.com>
2024-08-12 12:13:41 +0600

    file stucture and monggose connect completed

---
+++ src/Db/connectDb.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const getConnectionString = () => {
+  let connectionUrl;
+
+  connectionUrl = process.env.DATABASE_LOCAL;
+  // console.log(connectionUrl);
+  connectionUrl = connectionUrl.replace("<username>", process.env.DB_USER);
+
+  connectionUrl = connectionUrl.replace("<password>", process.env.DB_PASS);
+  return connectionUrl;
+};
+
+const connectDB = async () => {
+  console.log("connection to database...");
+  const mongoURI = getConnectionString();
+  await mongoose.connect(mongoURI, { dbName: process.env.DB_NAME });
+  console.log("successfully connected to database!");m
+};
+
+module.exports = connectDB;
